59ba693ac7e8ad56a83aadc691d748a404b98c28
James Pace <james.a.pace@gmail.com>
2021-01-03 17:21:12 -0500

    Create ingestion component test

---
+++ src/test/java/to/parking/ComponentTest.java
@@ -0,0 +1,54 @@
+package to.parking;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import to.parking.utils.ElasticsearchExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class ComponentTest {
+
+    @RegisterExtension
+    final ElasticsearchExtension elasticsearchExtension = new ElasticsearchExtension();
+
+    private ApplicationContext applicationContext;
+    protected EmbeddedServer embeddedServer;
+    protected HttpClient httpClient;
+
+    @BeforeAll
+    void setUp() {
+        applicationContext = ApplicationContext.builder()
+            .properties(getProperties())
+            .build();
+        getSingletons().forEach(applicationContext::registerSingleton);
+        embeddedServer = applicationContext.start()
+            .getBean(EmbeddedServer.class)
+            .start();
+        httpClient = HttpClient.create(embeddedServer.getURL());
+    }
+
+    @AfterAll
+    void afterAll() {
+        httpClient.close();
+        embeddedServer.stop();
+    }
+
+    protected Map<String, Object> getProperties() {
+        return new HashMap<>();
+    }
+
+    protected List<Object> getSingletons() {
+        var singletons = new ArrayList<>();
+        singletons.add(elasticsearchExtension.getRestHighLevelClient());
+        return singletons;
+    }
+}
---
+++ src/test/java/to/parking/IngestionComponentTest.java
@@ -0,0 +1,30 @@
+package to.parking;
+
+import io.micronaut.http.HttpStatus;
+import org.elasticsearch.search.SearchHit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import to.parking.ComponentTest;
+import to.parking.fixture.ParkingDataFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IngestionComponentTest extends ComponentTest {
+
+    @Test
+    @DisplayName("Can successfully ingest a CSV file")
+    void canSuccessfullyIngestACsvFile() {
+
+        var fixture = ParkingDataFixture.standard();
+
+        var httpResponse = httpClient.toBlocking().exchange(fixture.getIngestionRequest());
+
+        assertThat((Object) httpResponse.getStatus()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        var searchHits = elasticsearchExtension.readAllFromIndex("to-parking");
+        assertThat(searchHits)
+            .extracting(SearchHit::getSourceAsMap)
+            .asList()
+            .containsExactlyElementsOf(fixture.getParkingSpotDocuments());
+    }
+}
